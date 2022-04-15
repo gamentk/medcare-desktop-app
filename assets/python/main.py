84501bccf4269b9ff4e874f786d1ffde2a91c2e8
gamentk <nattakorn6090@gmail.com>
2022-04-15 19:23:03 +0700

    test

--- assets/python/main.py
+++ assets/python/main.py
@@ -2,7 +2,23 @@ import RPi.GPIO as GPIO
 import time
 
 from ultrasonic import distance
-import servo
+
+GPIO_SERVO = int('11')
+
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(GPIO_SERVO, GPIO.OUT)
+
+servo = GPIO.PWM(GPIO_SERVO, 50)
+
+
+def start():
+    servo.start(2)
+    servo.ChangeDutyCycle(2)
+
+
+def stop():
+    servo.stop()
+
 
 if __name__ == '__main__':
     try:
