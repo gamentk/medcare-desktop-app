1d6c19a9a9b76e5b346f1d7d753473cc9de3061e
gamentk <nattakorn6090@gmail.com>
2022-04-15 19:19:13 +0700

    z

--- assets/python/main.py
+++ assets/python/main.py
@@ -1,19 +1,25 @@
+import sys
 import RPi.GPIO as GPIO
 import time
 
 from ultrasonic import distance
 import servo
 
+GPIO_SERVO = int(sys.argv[1])
+GPIO.setup(GPIO_SERVO, GPIO.OUT)
+
+servo = GPIO.PWM(GPIO_SERVO, 50)
+
 if __name__ == '__main__':
     try:
         while True:
-            servo.start()
+            servo.start(servo)
 
             dist = distance()
             print("%.1f" % dist)
 
             if dist <= 4:
-                servo.stop()
+                servo.stop(servo)
                 break
 
             time.sleep(0.03)
--- assets/python/servo.py
+++ assets/python/servo.py
@@ -1,17 +1,7 @@
-import RPi.GPIO as GPIO
-
-GPIO_SERVO = 11
-
-GPIO.setmode(GPIO.BOARD)
-GPIO.setup(GPIO_SERVO, GPIO.OUT)
-
-servo = GPIO.PWM(GPIO_SERVO, 50)
-
-
-def start():
+def start(servo):
     servo.start(2)
     servo.ChangeDutyCycle(2)
 
 
-def stop():
+def stop(servo):
     servo.stop()
